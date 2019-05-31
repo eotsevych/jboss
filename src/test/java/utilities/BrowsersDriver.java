512c1d6a0e12b851cd2ae29bc236e7fefe775f2c
Evgeniy Otsevich <evgeniy.otsevich@gmail.com>
2019-05-31 15:26:58 +0300

    grid

--- src/test/java/utilities/BrowsersDriver.java
+++ src/test/java/utilities/BrowsersDriver.java
@@ -3,11 +3,25 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BrowsersDriver {
 
-    protected WebDriver driver = returnDriver();
+    protected WebDriver driver;
+
+    {
+        try {
+            driver = new RemoteWebDriver(
+                        new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 
     private WebDriver returnDriver() {
         if (driver == null) {
